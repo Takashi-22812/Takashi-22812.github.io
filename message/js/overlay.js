35a62496ed0b29e75c79e2609660bae8afe48bfb
「Takashi Oreki」 <31328512+Takashi-22812@users.noreply.github.com>
2018-07-05 23:21:40 -0700

    Delete overlay.js

--- message/js/overlay.js
+++
@@ -1,9 +0,0 @@
-$(window).on("load", function(){
-	$("#overlay").hide();
-	$("#openBtn").click(function(){
-		$("#overlay").fadeIn(700);
-	});
-	$("#closeBtn").click(function(){
-		$("#overlay").fadeOut(1000);
-});
-});
